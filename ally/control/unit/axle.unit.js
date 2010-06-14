ac939e525055591c5fea4e53dde07882a8bfe350
Cefn Hoile <allyourx.github@cefn.com>
2010-06-14 01:30:59 +0100

    Verified a few more unit tests

--- ally/control/unit/axle.unit.js
+++ ally/control/unit/axle.unit.js
@@ -14,30 +14,44 @@ $(function(){
 		}],
 		["Keydown left angle-bracket where editable creates and focuses element", function(){
 			var evt = $.Event('keypress');
-			evt.which = 60; // '<' character
+			evt.which = '<'.charCodeAt(0); // '<' character
 			$("*[contenteditable=true]").trigger(evt);
 			return UNITTEST.thingy.getChildren().length === 1 && $(document.activeElement).hasClass("xname"); //add test for focus [document.activeElement]
 		}],		
 		["Keydown alpha modifies element name", function(){
 			var evt = $.Event('keypress');
-			evt.which = 97;
+			evt.which = 'a'.charCodeAt(0);
 			$("*[contenteditable=true]").trigger(evt);
 			var name = UNITTEST.thingy.getChildren()[0].getName();
-			return name ==="a"; //fails because focus event bubbles and multiple focus handlers are watching 
+			return name ==="a"; 
 		}],
 		["Keydown space creates and focuses attribute name", function(){
-			return false;
+			var evt = $.Event('keypress');
+			evt.which = ' '.charCodeAt(0);
+			$("*[contenteditable=true]").trigger(evt);
+			var atts = UNITTEST.thingy.getChildren()[0].getAttributes();
+			return "" in atts && atts[""] instanceof YOURX.AttributeThingy; 
 		}],
 		["Keydown alpha modifies attribute value", function(){
-			return false;
+			var evt = $.Event('keypress');
+			evt.which = 'b'.charCodeAt(0);
+			$("*[contenteditable=true]").trigger(evt);
+			var atts = UNITTEST.thingy.getChildren()[0].getAttributes();
+			return "" in atts && atts[""].name === "b"; //TODO have to have elements tracking attribute names so attribute maps are up to date 
 		}],
 		["Keydown equals moves focus to attribute value", function(){
+			var evt = $.Event('keypress');
+			evt.which = '='.charCodeAt(0);
+			$("*[contenteditable=true]").trigger(evt);
 			return false;
 		}],
 		["Keydown quote returns focus to open tag", function(){
 			return false;
 		}],
-		["Keydown right angle-bracket moves focus to descendants", function(){
+		["Keydown right angle-bracket moves focus to available descendants", function(){
+			return false;
+		}],
+		["Keydown right angle-bracket moves focus to parent when descendants exhausted", function(){
 			return false;
 		}],
 		["Keydown backspace on structural character deletes structure", function(){
@@ -53,6 +67,24 @@ $(function(){
 			return false;
 		}],
 		["Validating interactive; Keydown angle-bracket prompts operations ", function(){
+			return false;
+		}],
+		["Click on element name sets focus", function(){
+			return false;
+		}],
+		["Click on attribute name sets focus", function(){
+			return false;
+		}],
+		["Click on attribute value sets focus", function(){
+			return false;
+		}],
+		["Click in text sets focus", function(){
+			return false;
+		}],
+		["Click after last attribute in an element sets focus", function(){
+			return false;
+		}],
+		["Click after element open tag in an element sets focus in descendant area", function(){
 			return false;
 		}],
 	]);
