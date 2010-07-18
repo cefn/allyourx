2630802d6fc10d6071279f15f5cea581ddd3f0ec
Cefn Hoile <allyourx.github@cefn.com>
2010-07-18 03:49:39 +0100

    Noted need to protect global namespace in Unit testing

--- allyourx/test/unit.js
+++ allyourx/test/unit.js
@@ -1,3 +1,5 @@
+//TODO protect test code with closure and namespace
+
 function AssertException(message) { this.message = message; }
 AssertException.prototype.toString = function () {
   return 'AssertException: ' + this.message;
