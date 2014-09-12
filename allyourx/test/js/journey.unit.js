bf7cc131b72b6753285271e08bb85dab33a47345
Cefn Hoile <inventor@cefn.com>
2014-09-12 18:42:58 +0100

    Introduced initial example grammar and validation for Journey domain example.

--- allyourx/test/js/journey.unit.js
+++ allyourx/test/js/journey.unit.js
@@ -9,13 +9,21 @@ $(function() {
             UNITTEST.grammar = YOURX.ThingyUtil.url2rule("../cases/journey/journey.001.rng");
             return (UNITTEST.grammar) instanceof YOURX.ThingyGrammar;
         }],
-        ["Message",function(){
-            //steps
-            ;
+        ["load data from (valid) journey.001.xml to in-memory model", function(){
+            UNITTEST.thingy = YOURX.ThingyUtil.url2thingy("../cases/journey/journey.001.xml");
+            return ( (UNITTEST.thingy) instanceof YOURX.RootThingy) ;
+            //TODO add checks for document structure like in yourx.unit.js?
         }],
-        ["Message",function(){
-            //steps
-            ;
+        ["journey.001.xml accepted by validation",function(){
+            return UNITTEST.grammar.matchThingy(UNITTEST.thingy) === true;
+        }],
+        ["load data from (invalid) journey.001a.xml to in-memory model", function(){
+            UNITTEST.thingy = YOURX.ThingyUtil.url2thingy("../cases/journey/journey.001a.xml");
+            return ( (UNITTEST.thingy) instanceof YOURX.RootThingy) ;
+            //TODO add checks for document structure like in yourx.unit.js?
+        }],
+        ["journey.001a.xml rejected by validation",function(){
+            return UNITTEST.grammar.matchThingy(UNITTEST.thingy) === false;
         }],
         ["Message",function(){
             //steps
